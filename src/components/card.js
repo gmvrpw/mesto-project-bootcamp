d32e63a09bd5c5f0307475e6d64cfc635d1634b1
gmvrpw <dev@gmvr.pw>
2023-04-29 22:41:31 +0300

    Добавляет синхронизацию с api для карточек

--- src/components/card.js
+++ src/components/card.js
@@ -1,32 +1,62 @@
 import { openPopup } from "./modal";
 import { setPlaceToPlacePopup } from "./modal/place";
+import Api from "./api";
 
 const placePopup = document.querySelector("#place-popup");
 const cardTemplate = document.querySelector("#card-template");
 
-export const createCard = ({name, link}) => {
-  const card = cardTemplate.content.querySelector(".card").cloneNode(true);
+export const likeCard = (cardLikeButton) => {
+  cardLikeButton.classList.add("card__like_liked");
+}
+
+export const dislikeCard = (cardLikeButton) => {
+  cardLikeButton.classList.remove("card__like_liked");
+}
+
+export const showDeleteButton = (cardDeleteButton) => {
+  cardDeleteButton.classList.remove("card__delete_hidden");
+}
+
+export const createCard = async (card) => {
+  const cardElement = cardTemplate.content.querySelector(".card").cloneNode(true);
 
   // Элементы
-  const cardImage = card.querySelector(".card__image");
-  const cardName = card.querySelector(".card__name");
-  const cardLikeButton = card.querySelector(".card__like")
-  const cardDeleteButton = card.querySelector(".card__delete")
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardName = cardElement.querySelector(".card__name");
+  const cardLikeButton = cardElement.querySelector(".card__like")
+  const cardLikeCounter = cardElement.querySelector(".card__like-counter")
+  const cardDeleteButton = cardElement.querySelector(".card__delete")
 
   // Данные
-  cardImage.src = link;
-  cardImage.alt = name;
-  cardName.textContent = name;
+  cardElement.dataset.id = card._id;
+  cardImage.src = card.link;
+  cardImage.alt = card.name;
+  cardName.textContent = card.name;
+  cardLikeCounter.textContent = card.likes.length;
+  if (card.likes.includes(Api.myId)) likeCard(cardLikeButton);
+  if (card.owner._id === Api.myId) showDeleteButton(cardDeleteButton);
 
   // События
-  cardLikeButton.addEventListener("click", () => cardLikeButton.classList.toggle("card__like_liked"))
-  cardDeleteButton.addEventListener("click", () => {
-    card.remove();
+  cardLikeButton.addEventListener("click", async () => {
+    if (cardLikeButton.classList.contains("card__like_liked")) {
+      await Api.dislikeCard(card._id);
+      dislikeCard(cardLikeButton);
+    } else {
+      await Api.likeCard(card._id);
+      likeCard(cardLikeButton);
+    }
+  })
+  cardDeleteButton.addEventListener("click", async () => {
+    await Api.deleteCard(card._id);
+    cardElement.remove();
   })
   cardImage.addEventListener("click", () => {
-    setPlaceToPlacePopup({name, link});
+    setPlaceToPlacePopup({
+      name: card.name,
+      link: card.link
+    });
     openPopup(placePopup);
   })
 
-  return card;
+  return cardElement;
 }
