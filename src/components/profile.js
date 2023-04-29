3427e62472b2b08fe601d5f4aa9f2b998ab39fe5
gmvrpw <dev@gmvr.pw>
2023-04-29 22:07:43 +0300

    Добавляет класс api со всеми доступными методами

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,15 +1,9 @@
-import { request } from "./net";
-
 const profileElement = document.querySelector(".profile");
 const profileAvatar = profileElement.querySelector(".profile__avatar");
 const profileName = profileElement.querySelector(".profile__name");
 const profileAbout = profileElement.querySelector(".profile__about");
 
-export const setProfile = (profile, sync) => {
-  if (sync) {
-    profile = uploadProfile(profile);
-  }
-
+export const setProfile = (profile) => {
   if (profile.avatar) profileAvatar.src = profile.avatar;
   if (profile.about) profileAbout.textContent = profile.about;
   if (profile.name) profileName.textContent = profile.name;
@@ -21,23 +15,4 @@ export const getProfile = () => {
     name: profileName.textContent,
     about: profileAbout.textContent
   }
-}
-
-export const loadProfile = async () => {
-  const res = await request({
-    method: "GET",
-    path: "/users/me",
-  })
-
-  setProfile(await res.json());
-}
-
-export const uploadProfile = async (profile) => {
-  const res = await request({
-    path: "/users/me",
-    method: "PATCH",
-    body: JSON.stringify(profile)
-  })
-
-  return await res.json();
 }
