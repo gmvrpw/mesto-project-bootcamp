32820f23fb15e1130a5cc2cf3ac4dcd4316cabba
gmvrpw <dev@gmvr.pw>
2023-04-29 18:35:21 +0300

    Добавляет синхронизацию загрузки и изменения профиля с сервером

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,10 +1,26 @@
-const profile = document.querySelector(".profile");
-const profileName = profile.querySelector(".profile__name");
-const profileDescription = profile.querySelector(".profile__description");
+import { request } from "./net";
 
-export const setProfile = ({name, description}) => {
-  profileName.textContent = name;
-  profileDescription.textContent = description;
+const profileElement = document.querySelector(".profile");
+const profileAvatar = profileElement.querySelector(".profile__avatar");
+const profileName = profileElement.querySelector(".profile__name");
+const profileDescription = profileElement.querySelector(".profile__description");
+
+export const setProfileAvatar = (url, sync = true) => {
+  if (sync) {
+    url = uploadProfile({avatar: url}).url;
+  }
+  profileAvatar.src = url;
+}
+
+export const setProfile = (profile, sync = true) => {
+  if (sync) {
+    profile = uploadProfile({
+      name,
+      about: profile.description
+    })
+  }
+  profileName.textContent = profile.name;
+  profileDescription.textContent = profile.description;
 }
 
 export const getProfile = () => {
@@ -12,4 +28,26 @@ export const getProfile = () => {
     name: profileName.textContent,
     description: profileDescription.textContent
   }
+}
+
+export const loadProfile = async () => {
+  const res = await request({
+    method: "GET",
+    path: "/users/me",
+  })
+
+  const {name, about, avatar} = await res.json();
+
+  setProfileAvatar(avatar);
+  setProfile({name, description: about});
+}
+
+export const uploadProfile = async (profile) => {
+  const res = await request({
+    path: "/users/me",
+    method: "PATCH",
+    body: JSON.stringify(profile)
+  })
+
+  return await res.json();
 }
