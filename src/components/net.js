013437326243cdabf14ad2b58741f8711deba2dd
gmvrpw <dev@gmvr.pw>
2023-04-29 19:01:19 +0300

    Исправляет пути до ресурса

--- src/components/net.js
+++ src/components/net.js
@@ -1,5 +1,5 @@
 export const request = ({method, path, body}) => {
-  return fetch(`https://nomoreparties.co/v1/${process.env.GROUP_ID}/${path}`, {
+  return fetch(`https://nomoreparties.co/v1/${process.env.GROUP_ID}${path}`, {
     method: method,
     headers: {
       authorization: process.env.TOKEN,
